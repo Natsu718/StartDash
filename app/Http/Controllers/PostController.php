e54e01a0b49b6f51d6254f9b4f2908450b1c8e17
kanon0884 <kanon.onepin@gmail.com>
2023-07-08 18:16:15 +0900

    Cloudinaryのインストール

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Post;
 use App\Models\Category;
+use Cloudinary:
 
 class PostController extends Controller
 {
@@ -25,6 +26,7 @@ class PostController extends Controller
 
     public function store(Post $post, Request $request)
     {
+         $image_url = Cloudinary::upload($request->file('image')->getRealPath())->getSecurePath();
         $input = $request['post'];
         $post->fill($input)->save();
         return redirect('/posts/' . $post->id);
