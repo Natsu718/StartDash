f3cdff50af2c02d5f3a731cb2242215991c1aee7
kanon0884 <kanon.onepin@gmail.com>
2023-07-08 19:47:48 +0900

    ルーティングの作成

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\PostController;
+App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,6 +23,19 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::controller(PostController::class)->middleware(['auth'])->group(function(){
+    Route::get('/', 'grade_index'])->name('grade_index');
+    Route::get('/classes', 'class_index'])->name('class_index');
+    Route::get('/names', 'name_index'])->name('name_index');
+    Route::get('/index', 'index'])->name('index');
+    Route::get('/posts/create','create'])->name('create');
+    Route::get('/posts/{post}','show'])->name('show');
+    Route::post('/posts','store'])->name('store');
+    Route::get('/posts/{post}/edit', 'edit'])->name('edit');
+    Route::put('/posts/{post}', 'update'])->name('update');
+    Route::delete('/posts/{post}','delete'])->name('delete');
+})
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
