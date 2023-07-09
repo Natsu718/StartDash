6f8042fec1b7b28259d57ee55260324319708d97
Two-key <21ue006@mb2.fwu.ac.jp>
2023-07-09 14:44:45 +0900

    grade, name, indexファイルの編集

--- resources/views/posts/name_index.blade.php
+++ resources/views/posts/name_index.blade.php
@@ -2,20 +2,15 @@
     <x-slot name="header">
         
     </x-slot>
-        <h1>チーム開発会へようこそ！</h1>
-        <h2>投稿一覧画面</h2>
-        <a href='/posts/create'>新規投稿</a>
+        <h1>{{ $classx->class }}</h1>
+        <h2>だれの日記を見る？</h2>
         <div>
             @foreach ($posts as $post)
                 <div style='border:solid 1px; margin-bottom: 10px;'>
                     <p>
                         タイトル：<a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
                     </p>
-                    <p>カテゴリー：<a href="/categories/{{ $post->category->id }}">{{ $post->category->name }}</a></p>
                 </div>
             @endforeach
-        </div>
-        <div>
-            {{ $posts->links() }}
         </div>
 </x-app-layout>
